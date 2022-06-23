791a981fc02bf8e6c2dbaf644b18e97bb56c5cb5
sdw1972 <sdw1972.cr@gmail.com>
2022-06-23 20:46:23 +0100

    Improved Layout

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,59 +1,42 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout')
 
-        <!-- Bootstrap CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+@section('content')
 
-        <title>Shopping list demo</title>
-    </head>
-    <body>
-    <section class="vh-100" style="background-color: #eee;">
-      <div class="container py-5 h-100">
-        <div class="row d-flex justify-content-center align-items-center h-100">
-          <div class="col col-lg-9 col-xl-7">
-            <div class="card rounded-3">
-              <div class="card-body p-4">
-
-                <h4 class="text-center my-3 pb-3">Shopping list Demo</h4>
+    <h4 class="text-center my-3 pb-3">Shopping list Demo</h4>
             
-                @if ($errors->any())
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
 
-                @if (session('status'))
-                    <div class="alert alert-success">
-                        {{ session('status') }}
-                    </div>
-                @endif
+        @if (session('status'))
+            <div class="alert alert-success">
+                {{ session('status') }}
+            </div>
+        @endif
 
-                <form action="{{ url('/purchases') }}" method="POST" class="row justify-content-center align-items-center mb-4 pb-2">
-                @csrf  
-                
-                    <div class="form-group col-md-8">
-                        <input type="text" class="form-control" name="item" placeholder="Add an item to the list"/>
-                    </div>
+        <form action="{{ url('/purchases') }}" method="POST" class="row justify-content-center align-items-center mb-4 pb-2">
+        @csrf  
+        <div class="form-group col-md-8">
+            <input type="text" class="form-control" name="item" placeholder="Add an item to the list"/>
+        </div>
                     
-                    <div class="form-group col-md-2">
-                        <input type="text" class="form-control" name="quantity" placeholder="Quantity"/>
-                    </div>
+        <div class="form-group col-md-2">
+            <input type="text" class="form-control" name="quantity" placeholder="Quantity"/>
+        </div>
                    
-                    <div class="form-group col-md-2">
-                        <button type="submit" class="btn btn-primary">Save</button>
-                    </div>
+        <div class="form-group col-md-2">
+            <button type="submit" class="btn btn-primary">Save</button>
+        </div>
             
-                </form>
+        </form>
 
-                <table>
+            <table>
 
                 @foreach($shoppingList as $thing)
                 <tr>
@@ -72,14 +55,6 @@
                         </form>
                 </tr>
                 @endforeach
-                </table>
+            </table>
          
-          </div>
-        </div>
-       </div>
-      </div>
-    </div>
-  </section>
-
-  </body>
-</html>
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+        <title>Shopping list demo</title>
+    </head>
+    <body>
+        <section class="vh-100" style="background-color: #eee;">
+            <div class="container py-5 h-100">
+                <div class="row d-flex justify-content-center align-items-center h-100">
+                    <div class="col col-lg-9 col-xl-7">
+                        <div class="card rounded-3">
+                            <div class="card-body p-4">   
+
+                                @yield('content')
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </body>
+</html>
